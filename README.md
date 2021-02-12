74c044047703c5208ecbfafdb1a0ea3a4184ba58
mdmostafa <mdmostafaalmahmud@gmail.com>
2021-02-12 18:28:06 +0600

    token make and verify

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ $ npm install @mdmostafa/simplify
 ## Usage
 
 ```js
-const makeString = require("@mdmostafa/simplify");
+const { makeString, makeToken, verifyToken } = require("@mdmostafa/simplify");
 
 makeString("Bangla desh");
 //=> "Bangladesh!"
@@ -23,4 +23,18 @@ makeString(1234);
 //=> Uncaught TypeError: makeString wants a string!
 //    at tiny (<anonymous>:2:41)
 //    at <anonymous>:1:1
+
+makeToken("bangladesh", "secretKey");
+//=> 331d261003455b2b3659af7f8c046a4dc96a9bc0d0692d714cff35775e5a25aa
+
+makeToken("", "secretKey");
+makeToken("bangladesh", "");
+//=> TypeError: hash wants a string and secret key!
+
+verifyToken("bangladesh", "secretKey");
+//=> Your key is valid
+
+verifyToken("bangladesh", "abcd");
+//=> Your key/string is not valid
+
 ```
--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-module.exports = function makeString(str) {
+// Dependencies
+const crypto = require('crypto');
+
+// Module scaffolding
+const functions = {};
+
+// Remove empty space in string
+functions.makeString = (str) => {
     if (typeof str !== "string") throw new TypeError("MakeString wants a string!");
     return str.replace(/\s/g, "");
   };
+
+// Make token by hashing the string with signature
+// Store into local storage
+functions.makeToken = (str, secretKey) => {
+    if (typeof str === 'string' && str.length > 0 && typeof secretKey === 'string' && secretKey.length > 0) {
+        const hash = crypto
+            .createHmac('sha256', secretKey)
+            .update(str)
+            .digest('hex');
+            if (typeof window !== 'undefined') {
+                localStorage.setItem('simplifyToken', hash);
+            } else {
+                token = hash;
+            }
+            return hash;
+    } else {
+        throw new TypeError("hash wants a string and secret key!");
+    }
+};
+
+let token = '';
+
+// Verify token by the selected string with signature
+// Compare with local storage
+functions.verifyToken = (str, secretKey) => {
+    if (typeof str === 'string' && str.length > 0 && typeof secretKey === 'string' && secretKey.length > 0) {
+        const hash = crypto
+            .createHmac('sha256', secretKey)
+            .update(str)
+            .digest('hex');
+            if (typeof window !== 'undefined') {
+                const tokenId = localStorage.getItem('simplifyToken');
+
+                if (tokenId === hash) return 'Your key is valid'; 
+                return 'Your key/string is not valid';
+            } else {
+                if (token === hash) return 'Your key is valid'; 
+                return 'Your key/string is not valid';
+            }
+            
+    } else {
+        throw new TypeError("hash wants a string and secret key!");
+    }
+};
+
+// console.log(functions.makeToken("bangladesh", "secretKey"));
+// console.log(functions.verifyToken("bangladesh", "abcd"));
+// console.log(global.localStorage)
+module.exports = functions;
